4d4d9dd32217ba4c3514a736827d2b611669a45d
likaiqing <likaiqing@panda.tv>
2016-11-17 17:56:17 +0800

    pandaprocessor edit sleeptime

--- src/main/java/com/pandatv/common/PandaProcessor.java
+++ src/main/java/com/pandatv/common/PandaProcessor.java
@@ -8,7 +8,7 @@ import us.codecraft.webmagic.processor.PageProcessor;
  */
 public abstract class PandaProcessor implements PageProcessor {
     protected Site site = Site.me()
-            .setSleepTime(3000)
+            .setSleepTime(300)
             .setUseGzip(true)
             .setRetryTimes(5)
             .setTimeOut(5000)
