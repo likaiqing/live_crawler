89cb21968d24a2cede06dd74d1852951569cdb23
likaiqing <likaiqing@panda.tv>
2016-11-28 14:55:42 +0800

    seleniumtest commit

--- src/main/java/com/pandatv/work/SeleniumProcessor.java
+++ src/main/java/com/pandatv/work/SeleniumProcessor.java
@@ -45,8 +45,8 @@ public class SeleniumProcessor extends PandaProcessor {
 //        String chromeDriverPath = "/home/likaiqing/hive-tool/chromedriver";
         String firUrl = "https://www.douyu.com/directory/all";
         String secUrl = "http://1212.ip138.com/ic.asp";
-        String chromeDriverPath = "/Users/likaiqing/space/panda/live_crawler/src/doc/mac/chromedriver";
-        Spider.create(new SeleniumProcessor()).thread(1).addPipeline(new ConsolePipeline()).addUrl(firUrl).setDownloader(new SeleniumDownloader(chromeDriverPath)).run();
+        String chromeDriverPath = "/data/tmp/crawler_driver/chromedriver";
+        Spider.create(new SeleniumProcessor()).thread(1).addPipeline(new ConsolePipeline()).addUrl(firUrl,secUrl).setDownloader(new SeleniumDownloader(chromeDriverPath)).run();
     }
 
     public static void main(String[] args) {
