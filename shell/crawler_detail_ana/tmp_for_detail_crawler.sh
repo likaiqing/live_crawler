95063d7b5fa648e042277a214ad30a3fbdc3b79b
likaiqing <likaiqing@panda.tv>
2016-12-08 13:32:55 +0800

    edit deploy.sh

--- shell/crawler_detail_ana/tmp_for_detail_crawler.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for d in $(seq -w 1 4)
-do 
-	sh /home/likaiqing/shell/crawler_detail_ana/tmp_crawler_detail.sh 2016120$d
-done 
--- shell/deploy.sh
+++ shell/deploy.sh
@@ -5,5 +5,5 @@ remote_dir=~/shell
 
 for sub_dir in crawler crawler_ana crawler_detail_ana
 do
-    rsync -auvz $origin_dir/$sub_dir/ $remote_dir/$sub_dir/
+    rsync -auvz $origin_dir/$sub_dir/ 10.110.16.33:$remote_dir/$sub_dir/
 done
