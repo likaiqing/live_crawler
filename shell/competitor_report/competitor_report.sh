847590b1f6186ea61738936ac7aaf5efd30cf70e
likaiqing <likaiqing@panda.tv>
2016-12-20 18:00:02 +0800

    category report com

--- shell/competitor_report/competitor_report.sh
+++ shell/competitor_report/competitor_report.sh
@@ -6,9 +6,9 @@ date=${date:=`date -d 'yesterday' +%Y%m%d`}
 cd `dirname $0`
 sh $(dirname $0)/crawler_day_anchor_ana.sh $date
 echo "crawler_day_anchor_ana.sh $date down"
-sh $(dirname $0)/crawler_day_plat_ana.sh $date
+sh $(dirname $0)/crawler_day_cate_ana.sh $date
 echo "crawler_day_cate_ana.sh $date down"
 sh $(dirname $0)/crawler_all_anchor_ana.sh $date
 echo "crawler_all_anchor_ana.sh $date down"
-sh $(dirname $0)/crawler_all_plat_ana.sh $date
+sh $(dirname $0)/crawler_all_cate_ana.sh $date
 echo "crawler_all_cate_ana.sh $date down"
--- shell/competitor_report/crawler_category_analyse.sh
+++ shell/competitor_report/crawler_category_analyse.sh
@@ -4,51 +4,51 @@ date=$1
 date=${date:=`date -d 'yesterday' +%Y%m%d`}
 sub_1_days=`date -d "-1day $date" +%Y%m%d`
 
-hive -e "
-insert overwrite table panda_competitor_result.category_day_analyse partition(par_date)
-SELECT
-  plat,
-  category,
-  pcu,
-  row_number()
-  OVER (PARTITION BY plat
-    ORDER BY pcu DESC)         pcu_rank,
-  duration                     duration,
-  row_number()
-  OVER (PARTITION BY plat
-    ORDER BY duration DESC)    duration_rank,
-  rec_times                    rectimes,
-  row_number()
-  OVER (PARTITION BY plat
-    ORDER BY rec_times DESC)   rectimes_rank,
-  followers                    fol,
-  row_number()
-  OVER (PARTITION BY plat
-    ORDER BY followers DESC)   fol_rank,
-  weight                       weight,
-  row_number()
-  OVER (PARTITION BY plat
-    ORDER BY weight DESC)      weight_rank,
-  0                            gift_users,
-  0                            gift_times,
-  0.0                          gift_value,
-  0                            giftvalue_rank,
-  0                            barrage_users,
-  0                            barrage_amount,
-  0                            barrage_rank,
-  new_anchors,
-  row_number()
-  OVER (PARTITION BY plat
-    ORDER BY new_anchors DESC) new_anchors_rank,
-  lives,
-  row_number()
-  OVER (PARTITION BY plat
-    ORDER BY lives DESC)       lives_rank,
-  '$date'
-FROM
-  panda_competitor.crawler_day_cate_analyse
-WHERE par_date = '$date';
-"
+#hive -e "
+#insert overwrite table panda_competitor_result.category_day_analyse partition(par_date)
+#SELECT
+#  plat,
+#  category,
+#  pcu,
+#  row_number()
+#  OVER (PARTITION BY plat
+#    ORDER BY pcu DESC)         pcu_rank,
+#  duration                     duration,
+#  row_number()
+#  OVER (PARTITION BY plat
+#    ORDER BY duration DESC)    duration_rank,
+#  rec_times                    rectimes,
+#  row_number()
+#  OVER (PARTITION BY plat
+#    ORDER BY rec_times DESC)   rectimes_rank,
+#  followers                    fol,
+#  row_number()
+#  OVER (PARTITION BY plat
+#    ORDER BY followers DESC)   fol_rank,
+#  weight                       weight,
+#  row_number()
+#  OVER (PARTITION BY plat
+#    ORDER BY weight DESC)      weight_rank,
+#  0                            gift_users,
+#  0                            gift_times,
+#  0.0                          gift_value,
+#  0                            giftvalue_rank,
+#  0                            barrage_users,
+#  0                            barrage_amount,
+#  0                            barrage_rank,
+#  new_anchors,
+#  row_number()
+#  OVER (PARTITION BY plat
+#    ORDER BY new_anchors DESC) new_anchors_rank,
+#  lives,
+#  row_number()
+#  OVER (PARTITION BY plat
+#    ORDER BY lives DESC)       lives_rank,
+#  '$date'
+#FROM
+#  panda_competitor.crawler_day_cate_analyse
+#WHERE par_date = '$date';
+#"
 
 hive -e "
 insert overwrite table panda_competitor_result.category_day_changed partition(par_date)
@@ -129,5 +129,57 @@ FROM
 
 hive -e "
 insert overwrite table panda_competitor_result.category_day_change_analyse partition(par_date)
-
+SELECT
+  c1.plat,
+  c1.category,
+  c1.new_pcu_rank-coalesce(c2.new_pcu_rank,0) pcu_rank_changed,
+  row_number() OVER (PARTITION BY c1.plat ORDER BY c1.new_pcu_rank-coalesce(c2.new_pcu_rank,0) DESC ) pcu_rank_change_rank,
+  c1.new_duration_rank-coalesce(c2.new_duration_rank,0) dur_rank_changed,
+  row_number() OVER (PARTITION BY c1.plat ORDER BY c1.new_duration_rank-coalesce(c2.new_duration_rank,0) DESC ) dur_rank_change_rank,
+  c1.new_rec_times_rank-coalesce(c2.new_rec_times_rank,0) rec_times_rank_changed,
+  row_number() OVER (PARTITION BY c1.plat ORDER BY c1.new_rec_times_rank-coalesce(c2.new_rec_times_rank,0) DESC ) rec_times_rank_change_rank,
+  c1.new_followers_rank-coalesce(c2.new_followers_rank,0) fol_rank_changed,
+  row_number() OVER (PARTITION BY c1.plat ORDER BY c1.new_followers_rank-coalesce(c2.new_followers_rank,0) DESC ) fol_rank_change_rank,
+  c1.new_weight_rank-coalesce(c2.new_weight_rank,0) weight_rank_changed,
+  row_number() OVER (PARTITION BY c1.plat ORDER BY c1.new_weight_rank-coalesce(c2.new_weight_rank,0) DESC ) weight_rank_change_rank,
+  0,
+  0,
+  c1.new_anchors_rank-coalesce(c2.new_anchors_rank,0) new_anchors_rank_changed,
+  row_number() OVER (PARTITION BY c1.plat ORDER BY c1.new_anchors_rank-coalesce(c2.new_anchors_rank,0) DESC ) new_anchors_rank_change_rank,
+  c1.lives_rank-coalesce(c2.lives_rank,0) lives_rank,
+  row_number() OVER (PARTITION BY c1.plat ORDER BY c1.lives_rank-coalesce(c2.lives_rank,0) DESC ) lives_rank_change_rank,
+  '$date'
+FROM
+  (
+    SELECT
+      plat,
+      category,
+      new_pcu_rank,
+      new_duration_rank,
+      new_rec_times_rank,
+      new_followers_rank,
+      new_weight_rank,
+      new_anchors_rank,
+      lives_rank
+    FROM
+      panda_competitor.crawler_all_cate_analyse
+    WHERE par_date = '$date'
+  ) c1
+  LEFT JOIN
+  (
+    SELECT
+      plat,
+      category,
+      new_pcu_rank,
+      new_duration_rank,
+      new_rec_times_rank,
+      new_followers_rank,
+      new_weight_rank,
+      new_anchors_rank,
+      lives_rank
+    FROM
+      panda_competitor.crawler_all_cate_analyse
+    WHERE par_date = '$sub_1_days'
+  ) c2
+  ON c1.plat=c2.plat AND c1.category=c2.category;
 "
