9c47d8add15cadf259483c4a1f283d2b826fdd13
likaiqing <likaiqing@panda.tv>
2016-12-20 17:18:00 +0800

    add rank

--- shell/competitor_report/crawler_all_anchor_ana.sh
+++ shell/competitor_report/crawler_all_anchor_ana.sh
@@ -7,73 +7,107 @@ sub_1_days=`date -d "-1days $date" +%Y%m%d`
 hive -e "
 insert overwrite table panda_competitor.crawler_all_anchor_analyse partition(par_date)
 SELECT
-  coalesce(all_anc.rid, day_anc.rid)                                    rid,
-  coalesce(all_anc.plat, day_anc.plat)                                  plat,
-  coalesce(all_anc.category, day_anc.category)                          category,
-  coalesce(day_anc.pcu, all_anc.new_pcu, 0)                             new_pcu,
-  CASE WHEN coalesce(all_anc.max_pcu, 0) > coalesce(day_anc.pcu, 0)
-    THEN all_anc.max_pcu
-  ELSE day_anc.pcu END                                                  max_pcu,
-  coalesce(day_anc.weight, all_anc.new_weight, 0)                       new_weight,
-  CASE WHEN coalesce(all_anc.max_weight, 0) > coalesce(day_anc.weight, 0)
-    THEN all_anc.max_weight
-  ELSE day_anc.weight END                                               max_weight,
-  coalesce(day_anc.followers, all_anc.new_followers, 0)                 new_followers,
-  CASE WHEN coalesce(all_anc.max_followers, 0) > coalesce(day_anc.followers, 0)
-    THEN all_anc.max_followers
-  ELSE day_anc.followers END                                            max_followers,
-  coalesce(day_anc.live_times, all_anc.new_live_times)                  new_live_times,
-  coalesce(all_anc.sum_live_times, 0) + coalesce(day_anc.live_times, 0) sum_live_times,
-  coalesce(day_anc.duration, all_anc.new_duration, 0)                   new_duration,
-  coalesce(all_anc.sum_duration, 0) + coalesce(day_anc.duration, 0)     sum_duration,
-  coalesce(day_anc.rec_times, all_anc.new_rec_times, 0)                 new_rec_times,
-  coalesce(all_anc.sum_rec_times, 0) + coalesce(day_anc.rec_times, 0)   sum_rec_times,
-  CASE WHEN dis.rid IS NULL THEN 1 ELSE 0 END is_new,
+  rid,
+  plat,
+  category,
+  new_pcu,
+  row_number() OVER (PARTITION BY plat ORDER BY new_pcu DESC ) pcu_plat_rank,
+  row_number() OVER (PARTITION BY plat,category ORDER BY new_pcu DESC ) pcu_cate_rank,
+  max_pcu,
+  new_weight,
+  row_number() OVER (PARTITION BY plat ORDER BY new_weight DESC ) weight_plat_rank,
+  row_number() OVER (PARTITION BY plat,category ORDER BY new_weight DESC ) weight_cate_rank,
+  max_weight,
+  new_followers,
+  row_number() OVER (PARTITION BY plat ORDER BY new_followers DESC ) fol_plat_rank,
+  row_number() OVER (PARTITION BY plat,category ORDER BY new_followers DESC ) fol_cate_rank,
+  max_followers,
+  new_live_times,
+  sum_live_times,
+  new_duration,
+  row_number() OVER (PARTITION BY plat ORDER BY new_duration DESC ) dur_plat_rank,
+  row_number() OVER (PARTITION BY plat,category ORDER BY new_duration DESC ) dur_cate_rank,
+  sum_duration,
+  new_rec_times,
+  row_number() OVER (PARTITION BY plat ORDER BY new_rec_times DESC ) rectimes_plat_rank,
+  row_number() OVER (PARTITION BY plat,category ORDER BY new_rec_times DESC ) rectimes_cate_rank,
+  sum_rec_times,
+  is_new,
   '$date'
 FROM
   (
     SELECT
-      rid,
-      plat,
-      category,
-      pcu,
-      live_times,
-      duration,
-      weight,
-      followers,
-      rec_times
+      coalesce(all_anc.rid, day_anc.rid)                                    rid,
+      coalesce(all_anc.plat, day_anc.plat)                                  plat,
+      coalesce(all_anc.category, day_anc.category)                          category,
+      coalesce(day_anc.pcu, all_anc.new_pcu, 0)                             new_pcu,
+      CASE WHEN coalesce(all_anc.max_pcu, 0) > coalesce(day_anc.pcu, 0)
+        THEN all_anc.max_pcu
+      ELSE day_anc.pcu END                                                  max_pcu,
+      coalesce(day_anc.weight, all_anc.new_weight, 0)                       new_weight,
+      CASE WHEN coalesce(all_anc.max_weight, 0) > coalesce(day_anc.weight, 0)
+        THEN all_anc.max_weight
+      ELSE day_anc.weight END                                               max_weight,
+      coalesce(day_anc.followers, all_anc.new_followers, 0)                 new_followers,
+      CASE WHEN coalesce(all_anc.max_followers, 0) > coalesce(day_anc.followers, 0)
+        THEN all_anc.max_followers
+      ELSE day_anc.followers END                                            max_followers,
+      coalesce(day_anc.live_times, all_anc.new_live_times)                  new_live_times,
+      coalesce(all_anc.sum_live_times, 0) + coalesce(day_anc.live_times, 0) sum_live_times,
+      coalesce(day_anc.duration, all_anc.new_duration, 0)                   new_duration,
+      coalesce(all_anc.sum_duration, 0) + coalesce(day_anc.duration, 0)     sum_duration,
+      coalesce(day_anc.rec_times, all_anc.new_rec_times, 0)                 new_rec_times,
+      coalesce(all_anc.sum_rec_times, 0) + coalesce(day_anc.rec_times, 0)   sum_rec_times,
+      CASE WHEN dis.rid IS NULL
+        THEN 1
+      ELSE 0 END                                                            is_new
     FROM
-      panda_competitor.crawler_day_anchor_analyse
-    WHERE par_date = '$date'
-  ) day_anc
-  FULL JOIN
-  (
-    SELECT
-      rid,
-      plat,
-      category,
-      new_pcu,
-      max_pcu,
-      new_weight,
-      max_weight,
-      new_followers,
-      max_followers,
-      new_live_times,
-      sum_live_times,
-      new_duration,
-      sum_duration,
-      new_rec_times,
-      sum_rec_times
-    FROM
-      panda_competitor.crawler_all_anchor_analyse
-    WHERE par_date = '$sub_1_days'
-  ) all_anc
-    ON day_anc.rid = all_anc.rid AND day_anc.plat = all_anc.plat AND day_anc.category = all_anc.category
-  LEFT JOIN
-  (
-    SELECT distinct rid,plat
-    FROM panda_competitor.crawler_distinct_anchor
-    WHERE par_date = '$sub_1_days'
-  ) dis
-    ON day_anc.rid = dis.rid and day_anc.plat=dis.plat;
+      (
+        SELECT
+          rid,
+          plat,
+          category,
+          pcu,
+          live_times,
+          duration,
+          weight,
+          followers,
+          rec_times
+        FROM
+          panda_competitor.crawler_day_anchor_analyse
+        WHERE par_date = '$date'
+      ) day_anc
+      FULL JOIN
+      (
+        SELECT
+          rid,
+          plat,
+          category,
+          new_pcu,
+          max_pcu,
+          new_weight,
+          max_weight,
+          new_followers,
+          max_followers,
+          new_live_times,
+          sum_live_times,
+          new_duration,
+          sum_duration,
+          new_rec_times,
+          sum_rec_times
+        FROM
+          panda_competitor.crawler_all_anchor_analyse
+        WHERE par_date = '$sub_1_days'
+      ) all_anc
+        ON day_anc.rid = all_anc.rid AND day_anc.plat = all_anc.plat AND day_anc.category = all_anc.category
+      LEFT JOIN
+      (
+        SELECT DISTINCT
+          rid,
+          plat
+        FROM panda_competitor.crawler_distinct_anchor
+        WHERE par_date = '$sub_1_days'
+      ) dis
+        ON day_anc.rid = dis.rid AND day_anc.plat = dis.plat
+  )t;
 "
---
+++ shell/competitor_report/crawler_category_analyse.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+
+date=$1
+date=${date:=`date -d 'yesterday' +%Y%m%d`}
+sub_1_days=`date -d "-1day $date" +%Y%m%d`
+
+hive -e "
+insert overwrite table panda_competitor_result.category_day_analyse partition(par_date)
+SELECT
+  plat,
+  category,
+  pcu,
+  row_number()
+  OVER (PARTITION BY plat
+    ORDER BY pcu DESC)         pcu_rank,
+  duration                     duration,
+  row_number()
+  OVER (PARTITION BY plat
+    ORDER BY duration DESC)    duration_rank,
+  rec_times                    rectimes,
+  row_number()
+  OVER (PARTITION BY plat
+    ORDER BY rec_times DESC)   rectimes_rank,
+  followers                    fol,
+  row_number()
+  OVER (PARTITION BY plat
+    ORDER BY followers DESC)   fol_rank,
+  weight                       weight,
+  row_number()
+  OVER (PARTITION BY plat
+    ORDER BY weight DESC)      weight_rank,
+  0                            gift_users,
+  0                            gift_times,
+  0.0                          gift_value,
+  0                            giftvalue_rank,
+  0                            barrage_users,
+  0                            barrage_amount,
+  0                            barrage_rank,
+  new_anchors,
+  row_number()
+  OVER (PARTITION BY plat
+    ORDER BY new_anchors DESC) new_anchors_rank,
+  lives,
+  row_number()
+  OVER (PARTITION BY plat
+    ORDER BY lives DESC)       lives_rank,
+  '$date'
+FROM
+  panda_competitor.crawler_day_cate_analyse
+WHERE par_date = '$date';
+"
+
+hive -e "
+insert overwrite table panda_competitor_result.category_day_changed partition(par_date)
+SELECT
+  c1.plat,
+  c1.category,
+  c1.pcu - coalesce(c2.new_pcu, 0)                              pcu_changed,
+  row_number()
+  OVER (PARTITION BY c1.plat
+    ORDER BY c1.pcu - coalesce(c2.new_pcu, 0) DESC)             pcu_changed_rank,
+  c1.duration - coalesce(c2.new_duration, 0.0)                  duration_changed,
+  row_number()
+  OVER (PARTITION BY c1.plat
+    ORDER BY c1.duration - coalesce(c2.new_duration, 0.0) DESC) duration_change_rank,
+  c1.rec_times - coalesce(c2.new_rec_times, 0)                  rectimes_changed,
+  row_number()
+  OVER (PARTITION BY c1.plat
+    ORDER BY c1.rec_times - coalesce(c2.new_rec_times, 0) DESC) rectimes_change_rank,
+  c1.followers - coalesce(c2.new_followers, 0)                  fol_chaned,
+  row_number()
+  OVER (PARTITION BY c1.plat
+    ORDER BY c1.followers - coalesce(c2.new_followers, 0) DESC) fol_change_rank,
+  c1.weight - coalesce(c2.new_weight, 0.0)                      weight_changed,
+  row_number()
+  OVER (PARTITION BY c1.plat
+    ORDER BY c1.weight - coalesce(c2.new_weight, 0.0) DESC)     weight_change_rank,
+  0.0,
+  0,
+  0,
+  0,
+  c1.lives - coalesce(c2.lives, 0)                              activeanchors_changed,
+  row_number()
+  OVER (PARTITION BY c1.plat
+    ORDER BY c1.lives - coalesce(c2.lives, 0) DESC)             lives_change_rank,
+  c1.new_anchors - coalesce(c2.new_anchors, 0)                  newanchors_changed,
+  row_number()
+  OVER (PARTITION BY c1.plat
+    ORDER BY c1.new_anchors - coalesce(c2.new_anchors, 0) DESC) newanchors_change_rank,
+  '$date'
+FROM
+  (
+    SELECT
+      plat,
+      category,
+      pcu,
+      duration,
+      rec_times,
+      followers,
+      weight,
+      0.0,
+      0,
+      lives,
+      new_anchors
+    FROM
+      panda_competitor.crawler_day_cate_analyse
+    WHERE par_date = '$date'
+  ) c1
+  LEFT JOIN
+  (
+    SELECT
+      plat,
+      category,
+      new_pcu,
+      new_duration,
+      new_rec_times,
+      new_followers,
+      new_weight,
+      0.0,
+      0,
+      lives,
+      new_anchors
+    FROM
+      panda_competitor.crawler_all_cate_analyse
+    WHERE par_date = '$sub_1_days'
+  ) c2
+    ON c1.plat = c2.plat AND c1.category = c2.category;
+"
+
+hive -e "
+insert overwrite table panda_competitor_result.category_day_change_analyse partition(par_date)
+
+"
