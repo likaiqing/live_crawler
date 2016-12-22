8b786acbed738000a832fad745adbfa8f1e9d3fa
likaiqing <likaiqing@panda.tv>
2016-12-22 13:32:32 +0800

    day plat com

--- shell/competitor_report/crawler_day_plat_ana.sh
+++ shell/competitor_report/crawler_day_plat_ana.sh
@@ -7,105 +7,116 @@ sub_1_days=`date -d "-1day $date" +%Y%m%d`
 hive -e "
 insert overwrite table panda_competitor.crawler_day_plat_analyse partition(par_date)
 SELECT
-  coalesce(ana.plat, rec.plat)            plat,
-  coalesce(ana.max_pcu, rec.max_pcu)      max_pcu,
-  coalesce(ana.live_times, rec.rec_times) live_times,
-  coalesce(ana.duration, rec.duration)    duraion,
-  coalesce(ana.weight, rec.weight)        weight,
-  coalesce(ana.followers, rec.followers)  followers,
-  coalesce(rec.rec_times, 0)              rec_times,
-  anchors.lives                           lives,
-  anchors.new_anchors,
-  cates.categories,
-  cates.new_cates,
+  ana.plat,
+  ana.max_pcu,
+  ana.live_times,
+  ana.duraion,
+  ana.weight,
+  ana.followers,
+  ana.rec_times,
+  coalesce(anchors.lives, 0)       lives,
+  coalesce(anchors.new_anchors, 0) new_anchors,
+  coalesce(cates.categories, 0)    categories,
+  coalesce(cates.new_cates, 0)     new_cates,
   '$date'
 FROM
   (
     SELECT
-      coalesce(dur.plat, pcu.plat)             plat,
-      coalesce(dur.live_times, pcu.live_times) live_times,
-      coalesce(dur.duration, pcu.duration)     duration,
-      coalesce(dur.max_pcu, pcu.max_pcu)       max_pcu,
-      coalesce(pcu.weight, 0)                  weight,
-      coalesce(pcu.followers, 0)               followers
+      coalesce(ana.plat, rec.plat)            plat,
+      coalesce(ana.max_pcu, rec.max_pcu)      max_pcu,
+      coalesce(ana.live_times, rec.rec_times) live_times,
+      coalesce(ana.duration, rec.duration)    duraion,
+      coalesce(ana.weight, rec.weight)        weight,
+      coalesce(ana.followers, rec.followers)  followers,
+      coalesce(rec.rec_times, 0)              rec_times
     FROM
       (
         SELECT
-          plat,
-          sum(live_times)                    live_times,
-          round(sum(live_times) / 60 * 5, 2) duration,
-          max(pcu)                           max_pcu
+          coalesce(dur.plat, pcu.plat)             plat,
+          coalesce(dur.live_times, pcu.live_times) live_times,
+          coalesce(dur.duration, pcu.duration)     duration,
+          coalesce(dur.max_pcu, pcu.max_pcu)       max_pcu,
+          coalesce(pcu.weight, 0)                  weight,
+          coalesce(pcu.followers, 0)               followers
         FROM
           (
             SELECT
-              task_random,
-              split(task, 'anchor') [0] plat,
-              category,
-              count(DISTINCT rid)       live_times,
-              sum(populary_num)         pcu
-            FROM panda_competitor.crawler_anchor
-            WHERE par_date = '$date' AND task LIKE '%anchor' and category !=''
-            GROUP BY task_random, split(task, 'anchor') [0], category
-          ) t_r
-        GROUP BY plat
-      ) dur
+              plat,
+              sum(live_times)                    live_times,
+              round(sum(live_times) / 60 * 5, 2) duration,
+              max(pcu)                           max_pcu
+            FROM
+              (
+                SELECT
+                  task_random,
+                  split(task, 'anchor') [0] plat,
+                  category,
+                  count(DISTINCT rid)       live_times,
+                  sum(populary_num)         pcu
+                FROM panda_competitor.crawler_anchor
+                WHERE par_date = '$date' AND task LIKE '%anchor' AND category != ''
+                GROUP BY task_random, split(task, 'anchor') [0], category
+              ) t_r
+            GROUP BY plat
+          ) dur
+          FULL JOIN
+          (
+            SELECT
+              plat,
+              sum(anchors)                             live_times,
+              CASE WHEN plat = 'douyu'
+                THEN round(sum(anchors) / 60, 2)
+              ELSE round(sum(anchors) / 60 * 5, 2) END duration,
+              max(pcu)                                 max_pcu,
+              max(weight)                              weight,
+              max(followers)                           followers
+            FROM
+              (
+                SELECT
+                  task_random,
+                  split(task, 'detailanchor') [0] plat,
+                  category_sec                    category,
+                  count(DISTINCT rid,task_random) anchors,
+                  sum(online_num)                 pcu,
+                  sum(weight_num)                 weight,
+                  sum(follower_num)               followers
+                FROM
+                  panda_competitor.crawler_detail_anchor
+                WHERE par_date = '$date' AND task LIKE '%detailanchor' AND category_sec != ''
+                GROUP BY task_random, split(task, 'detailanchor') [0], category_sec
+              ) d
+            GROUP BY plat
+          ) pcu
+            ON dur.plat = pcu.plat
+      ) ana
       FULL JOIN
       (
         SELECT
           plat,
-          sum(anchors)                             live_times,
-          CASE WHEN plat = 'douyu'
-            THEN round(sum(anchors) / 60, 2)
-          ELSE round(sum(anchors) / 60 * 5, 2) END duration,
-          max(pcu)                                 max_pcu,
-          max(weight)                              weight,
-          max(followers)                           followers
+          sum(anchors)                    rec_times,
+          round(sum(anchors) / 60 * 5, 2) duration,
+          max(max_pcu)                    max_pcu,
+          max(weight)                     weight,
+          max(followers)                  followers
         FROM
           (
             SELECT
               task_random,
-              split(task, 'detailanchor') [0] plat,
+              split(task, 'index') [0]        plat,
               category_sec                    category,
               count(DISTINCT rid,task_random) anchors,
-              sum(online_num)                 pcu,
-              sum(weight_num)                 weight,
-              sum(follower_num)               followers
+              max(online_num)                 max_pcu,
+              max(weight_num)                 weight,
+              max(follower_num)               followers
             FROM
-              panda_competitor.crawler_detail_anchor
-            WHERE par_date = '$date' AND task LIKE '%detailanchor' and category_sec !=''
-            GROUP BY task_random, split(task, 'detailanchor') [0], category_sec
-          ) d
+              panda_competitor.crawler_indexrec_detail_anchor
+            WHERE par_date = '$date' AND task LIKE '%indexrec'
+            GROUP BY task_random, split(task, 'index') [0], category_sec
+          ) rec_tr
         GROUP BY plat
-      ) pcu
-        ON dur.plat = pcu.plat
+      ) rec
+        ON ana.plat = rec.plat
   ) ana
-  FULL JOIN
-  (
-    SELECT
-      plat,
-      sum(anchors)                    rec_times,
-      round(sum(anchors) / 60 * 5, 2) duration,
-      max(max_pcu)                    max_pcu,
-      max(weight)                     weight,
-      max(followers)                  followers
-    FROM
-      (
-        SELECT
-          task_random,
-          split(task, 'index') [0]        plat,
-          category_sec                    category,
-          count(DISTINCT rid,task_random) anchors,
-          max(online_num)                 max_pcu,
-          max(weight_num)                 weight,
-          max(follower_num)               followers
-        FROM
-          panda_competitor.crawler_indexrec_detail_anchor
-        WHERE par_date = '$date' AND task LIKE '%indexrec'
-        GROUP BY task_random, split(task, 'index') [0], category_sec
-      ) rec_tr
-    GROUP BY plat
-  ) rec
-    ON ana.plat = rec.plat
   LEFT JOIN
   (
     SELECT
@@ -127,14 +138,14 @@ FROM
               split(task, 'anchor') [0] plat,
               rid
             FROM panda_competitor.crawler_anchor
-            WHERE par_date = '$date' AND task LIKE '%anchor' and category !=''
+            WHERE par_date = '$date' AND task LIKE '%anchor' AND category != ''
             UNION ALL
             SELECT
               DISTINCT
               split(task, 'detailanchor') [0] plat,
               rid
             FROM panda_competitor.crawler_detail_anchor
-            WHERE par_date = '$date' AND task LIKE '%detailanchor' and category_sec !=''
+            WHERE par_date = '$date' AND task LIKE '%detailanchor' AND category_sec != ''
             UNION ALL
             SELECT
               DISTINCT
