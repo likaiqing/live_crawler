8ac75eb8326735958fdc2ce1c2adbca721b4b79b
likaiqing <likaiqing@panda.tv>
2016-12-22 12:54:38 +0800

    edit all anchor

--- shell/competitor_report/crawler_all_anchor_ana.sh
+++ shell/competitor_report/crawler_all_anchor_ana.sh
@@ -7,32 +7,54 @@ sub_1_days=`date -d "-1days $date" +%Y%m%d`
 hive -e "
 insert overwrite table panda_competitor.crawler_all_anchor_analyse partition(par_date)
 SELECT
-  rid,
-  plat,
+  day_anc.rid,
+  day_anc.plat,
   category,
   new_pcu,
-  row_number() OVER (PARTITION BY plat ORDER BY new_pcu DESC ) pcu_plat_rank,
-  row_number() OVER (PARTITION BY plat,category ORDER BY new_pcu DESC ) pcu_cate_rank,
+  row_number()
+  OVER (PARTITION BY day_anc.plat
+    ORDER BY new_pcu DESC)       pcu_plat_rank,
+  row_number()
+  OVER (PARTITION BY day_anc.plat, category
+    ORDER BY new_pcu DESC)       pcu_cate_rank,
   max_pcu,
   new_weight,
-  row_number() OVER (PARTITION BY plat ORDER BY new_weight DESC ) weight_plat_rank,
-  row_number() OVER (PARTITION BY plat,category ORDER BY new_weight DESC ) weight_cate_rank,
+  row_number()
+  OVER (PARTITION BY day_anc.plat
+    ORDER BY new_weight DESC)    weight_plat_rank,
+  row_number()
+  OVER (PARTITION BY day_anc.plat, category
+    ORDER BY new_weight DESC)    weight_cate_rank,
   max_weight,
   new_followers,
-  row_number() OVER (PARTITION BY plat ORDER BY new_followers DESC ) fol_plat_rank,
-  row_number() OVER (PARTITION BY plat,category ORDER BY new_followers DESC ) fol_cate_rank,
+  row_number()
+  OVER (PARTITION BY day_anc.plat
+    ORDER BY new_followers DESC) fol_plat_rank,
+  row_number()
+  OVER (PARTITION BY day_anc.plat, category
+    ORDER BY new_followers DESC) fol_cate_rank,
   max_followers,
   new_live_times,
   sum_live_times,
   new_duration,
-  row_number() OVER (PARTITION BY plat ORDER BY new_duration DESC ) dur_plat_rank,
-  row_number() OVER (PARTITION BY plat,category ORDER BY new_duration DESC ) dur_cate_rank,
+  row_number()
+  OVER (PARTITION BY day_anc.plat
+    ORDER BY new_duration DESC)  dur_plat_rank,
+  row_number()
+  OVER (PARTITION BY day_anc.plat, category
+    ORDER BY new_duration DESC)  dur_cate_rank,
   sum_duration,
   new_rec_times,
-  row_number() OVER (PARTITION BY plat ORDER BY new_rec_times DESC ) rectimes_plat_rank,
-  row_number() OVER (PARTITION BY plat,category ORDER BY new_rec_times DESC ) rectimes_cate_rank,
+  row_number()
+  OVER (PARTITION BY day_anc.plat
+    ORDER BY new_rec_times DESC) rectimes_plat_rank,
+  row_number()
+  OVER (PARTITION BY day_anc.plat, category
+    ORDER BY new_rec_times DESC) rectimes_cate_rank,
   sum_rec_times,
-  is_new,
+  CASE WHEN dis.rid IS NULL
+    THEN 1
+  ELSE 0 END                     is_new,
   '$date'
 FROM
   (
@@ -57,10 +79,7 @@ FROM
       coalesce(day_anc.duration, all_anc.new_duration, 0)                   new_duration,
       coalesce(all_anc.sum_duration, 0) + coalesce(day_anc.duration, 0)     sum_duration,
       coalesce(day_anc.rec_times, all_anc.new_rec_times, 0)                 new_rec_times,
-      coalesce(all_anc.sum_rec_times, 0) + coalesce(day_anc.rec_times, 0)   sum_rec_times,
-      CASE WHEN dis.rid IS NULL
-        THEN 1
-      ELSE 0 END                                                            is_new
+      coalesce(all_anc.sum_rec_times, 0) + coalesce(day_anc.rec_times, 0)   sum_rec_times
     FROM
       (
         SELECT
@@ -100,14 +119,14 @@ FROM
         WHERE par_date = '$sub_1_days'
       ) all_anc
         ON day_anc.rid = all_anc.rid AND day_anc.plat = all_anc.plat AND day_anc.category = all_anc.category
-      LEFT JOIN
-      (
-        SELECT DISTINCT
-          rid,
-          plat
-        FROM panda_competitor.crawler_distinct_anchor
-        WHERE par_date = '$sub_1_days'
-      ) dis
-        ON day_anc.rid = dis.rid AND day_anc.plat = dis.plat
-  )t;
+  ) day_anc
+  LEFT JOIN
+  (
+    SELECT DISTINCT
+      rid,
+      plat
+    FROM panda_competitor.crawler_distinct_anchor
+    WHERE par_date = '$sub_1_days'
+  ) dis
+    ON day_anc.rid = dis.rid AND day_anc.plat = dis.plat;
 "
