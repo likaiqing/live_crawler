3cf0cc81da83e989afc4a56ad908210f465f4fff
likaiqing <likaiqing@panda.tv>
2016-12-23 10:50:02 +0800

    huyaanchor edit

--- src/main/java/com/pandatv/processor/HuyaAnchorProcessor.java
+++ src/main/java/com/pandatv/processor/HuyaAnchorProcessor.java
@@ -23,18 +23,18 @@ import java.util.List;
  */
 public class HuyaAnchorProcessor extends PandaProcessor {
     private static final Logger logger = LoggerFactory.getLogger(HuyaAnchorProcessor.class);
-    private static String url = "http://www.huya.com/cache.php?m=Live&do=ajaxAllLiveByPage&pageNum=1&page=";
+//    private static String url = "http://www.huya.com/cache.php?m=Live&do=ajaxAllLiveByPage&pageNum=1&page=";
+    private static String url = "http://www.huya.com/cache.php?m=LiveList&do=getLiveListByPage&tagAll=0&page=";
 
     @Override
     public void process(Page page) {
         String url = page.getUrl().toString();
         logger.info("process url:{}", url);
-        List<String> all = page.getJson().jsonPath("$.data.list").all();
+        List<String> all = page.getJson().jsonPath("$.data.datas").all();
         if (all.size() > 0) {
             String newUrl = this.url + (Integer.parseInt(url.substring(url.lastIndexOf('=') + 1)) + 1);
             page.addTargetRequest(newUrl);
-            JSONArray list = JsonPath.read(page.getJson().get(), "$.data.list");
-            List<String> results = new ArrayList<>();
+            JSONArray list = JsonPath.read(page.getJson().get(), "$.data.datas");
             for (int i = 0; i < list.size(); i++) {
                 String jsonStr = list.get(i).toString();
                 String rid = JsonPath.read(jsonStr, "$.privateHost");
@@ -72,7 +72,7 @@ public class HuyaAnchorProcessor extends PandaProcessor {
             mailHours = args[3];
         }
         String hivePaht = Const.COMPETITORDIR + "crawler_anchor/" + date;
-        String firstUrl = "http://www.huya.com/cache.php?m=Live&do=ajaxAllLiveByPage&pageNum=1&page=1";
+        String firstUrl = "http://www.huya.com/cache.php?m=LiveList&do=getLiveListByPage&tagAll=0&page=1";
         Spider.create(new HuyaAnchorProcessor()).thread(1).addUrl(firstUrl).addPipeline(new ConsolePipeline()).setDownloader(new PandaDownloader()).run();
         CommonTools.writeAndMail(hivePaht, Const.HUYAFINISH, anchors);
     }
