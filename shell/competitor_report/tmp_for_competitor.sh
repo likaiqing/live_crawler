77656f5238a03cce565415a690a4c35b2994b34a
likaiqing <likaiqing@panda.tv>
2016-12-30 10:27:21 +0800

    fix data

--- shell/competitor_report/tmp_for_competitor.sh
+++ shell/competitor_report/tmp_for_competitor.sh
@@ -5,7 +5,13 @@
 #do
 #    sh $(dirname $0)/competitor_report.sh 201611$d
 #done
-for d in $(seq -w 20 22)
+for d in $(seq -w 20 28)
 do
-    sh $(dirname $0)/competitor_report.sh 201612$d
+    sh $(dirname $0)/crawler_day_cate_ana.sh 201612$d
+    sh $(dirname $0)/crawler_day_plat_ana.sh 201612$d
+    sh $(dirname $0)/crawler_all_cate_ana.sh 201612$d
+    sh $(dirname $0)/crawler_all_plat_ana.sh 201612$d
+    sh $(dirname $0)/crawler_category_analyse.sh 201612$d
+    sh $(dirname $0)/crawler_plat_analyse.sh 201612$d
+    sh $(dirname $0)/competitor_result_analyse.sh 201612$d
 done
