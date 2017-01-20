5abf5832084a2a500a4072e39c5b4a55fed710d0
likaiqing <likaiqing@panda.tv>
2017-01-20 17:07:27 +0800

    edit anchor_change_day.sh and seven_insert.sh

---
+++ shell/competitor_report/anchor_change_day.sh
@@ -0,0 +1,285 @@
+#!/bin/bash
+
+date=$1
+date=${date:=`date -d 'yesterday' +%Y%m%d`}
+date_sub=`date -d "-1day $date" +%Y%m%d`
+
+#主播日数据
+hive -e "
+insert overwrite table panda_competitor_result.crawler_anchor_day partition(par_date)
+SELECT
+  plat,
+  category,
+  rid,
+  name,
+  --主播昵称
+  pcu,
+  row_number()
+  OVER (PARTITION BY plat
+    ORDER BY pcu DESC),
+  row_number()
+  OVER (PARTITION BY plat, category
+    ORDER BY pcu DESC),
+  duration,
+  row_number()
+  OVER (PARTITION BY plat
+    ORDER BY duration DESC),
+  row_number()
+  OVER (PARTITION BY plat, category
+    ORDER BY duration DESC),
+  rec_times,
+  row_number()
+  OVER (PARTITION BY plat
+    ORDER BY rec_times DESC),
+  row_number()
+  OVER (PARTITION BY plat, category
+    ORDER BY rec_times DESC),
+  followers,
+  row_number()
+  OVER (PARTITION BY plat
+    ORDER BY followers DESC),
+  row_number()
+  OVER (PARTITION BY plat, category
+    ORDER BY followers DESC),
+  weight,
+  row_number()
+  OVER (PARTITION BY plat
+    ORDER BY weight DESC),
+  row_number()
+  OVER (PARTITION BY plat, category
+    ORDER BY weight DESC),
+  NULL,
+  --gift_users  赠送礼物人数
+  NULL,
+  ---gift_times 赠送礼物次数
+  NULL,
+  --gift_value  赠送礼物价值
+  NULL,
+  --giftvalue_plat_rank 平台礼物价值排名
+  NULL,
+  --giftvalue_cate_rank 版区礼物价值排名
+  NULL,
+  --barrage_users   发弹幕人数
+  NULL,
+  --barrage_amount  发弹幕数量
+  NULL,
+  --barrage_plat_rank   平台弹幕数排名
+  NULL,
+  --barrage_cate_rank   版区弹幕数排名
+  is_new,
+  --是否是新主播
+  room_content,
+  par_date
+FROM (
+       SELECT
+         a.par_date,
+         a.plat,
+         c.category,
+         a.rid,
+         b.name,
+         b.room_content,
+         min(a.is_new)    AS is_new,
+         max(a.pcu)       AS pcu,
+         sum(a.duration)  AS duration,
+         max(a.rec_times) AS rec_times,
+         max(a.followers) AS followers,
+         max(a.weight)    AS weight
+       FROM panda_competitor.crawler_day_anchor_analyse a
+         LEFT JOIN
+         (select rid,plat,name,title as room_content,
+row_number() over(partition by rid,plat order by create_time desc) as rw 
+from panda_competitor.crawler_distinct_anchor 
+where par_date='${date}') b
+           ON b.rw=1 and a.rid = b.rid AND a.plat = b.plat
+         LEFT JOIN
+         (SELECT
+            plat,
+            rid,
+            category,
+            pcu,
+            row_number()
+            OVER (PARTITION BY plat, rid
+              ORDER BY pcu DESC) AS rw
+          FROM panda_competitor.crawler_day_anchor_analyse
+          WHERE par_date = '${date}') c
+           ON a.plat = c.plat AND a.rid = c.rid AND c.rw = 1
+       WHERE a.par_date = '${date}'
+       GROUP BY a.par_date, a.plat, c.category, a.rid, b.name,b.room_content
+     ) zz;"
+
+
+hive -e "
+insert overwrite table panda_competitor_result.crawler_anchor_change_day partition(par_date)
+SELECT
+  aa.plat,
+  --    平台
+  aa.category,
+  --    版区
+  aa.rid,
+  --主播ID
+  aa.name,
+  --主播昵称
+  (aa.pcu - nvl(bb.pcu, 0)),
+  --pcu_changed PCU变化量
+  row_number()
+  OVER (PARTITION BY aa.plat
+    ORDER BY (aa.pcu - nvl(bb.pcu, 0)) DESC),
+  --pcu_changed_plat_rank
+  row_number()
+  OVER (PARTITION BY aa.plat, aa.category
+    ORDER BY (aa.pcu - nvl(bb.pcu, 0)) DESC),
+  --pcu_changed_cate_rank
+  (aa.duration - nvl(bb.duration, 0)),
+  --livetime_changed    直播时长变化量
+  row_number()
+  OVER (PARTITION BY aa.plat
+    ORDER BY (aa.duration - nvl(bb.duration, 0)) DESC),
+  --livetime_changed_plat_rank
+  row_number()
+  OVER (PARTITION BY aa.plat, aa.category
+    ORDER BY (aa.duration - nvl(bb.duration, 0)) DESC),
+  --livetime_changed_cate_rank
+  (aa.rec_times - nvl(bb.rec_times, 0)),
+  --rec_changed 推荐次数变化量
+  row_number()
+  OVER (PARTITION BY aa.plat
+    ORDER BY (aa.rec_times - nvl(bb.rec_times, 0)) DESC),
+  --rec_changed_plat_rank
+  row_number()
+  OVER (PARTITION BY aa.plat, aa.category
+    ORDER BY (aa.rec_times - nvl(bb.rec_times, 0)) DESC),
+  --rec_changed_cate_rank
+  (aa.followers - nvl(bb.followers, 0)),
+  --fol_changed 关注变化量
+  row_number()
+  OVER (PARTITION BY aa.plat
+    ORDER BY (aa.followers - nvl(bb.followers, 0)) DESC),
+  --fol_changed_plat_rank
+  row_number()
+  OVER (PARTITION BY aa.plat, aa.category
+    ORDER BY (aa.followers - nvl(bb.followers, 0)) DESC),
+  --fol_changed_cate_rank
+  (aa.weight - nvl(bb.weight, 0)),
+  --weight_changed  体重变化量
+  row_number()
+  OVER (PARTITION BY aa.plat
+    ORDER BY (aa.weight - nvl(bb.weight, 0)) DESC),
+  --weight_changed_plat_rank
+  row_number()
+  OVER (PARTITION BY aa.plat, aa.category
+    ORDER BY (aa.weight - nvl(bb.weight, 0)) DESC),
+  --weight_changed_cate_rank
+  NULL,
+  --giftvalue_changed   礼物价值变化量
+  NULL,
+  --giftvalue_changed_plat_rank
+  NULL,
+  --giftvalue_changed_cate_rank
+  NULL,
+  --barrage_changed 弹幕数变化量
+  NULL,
+  --barrage_changed_plat_rank
+  NULL,
+  --barrage_changed_cate_rank
+  room_content,
+  aa.par_date
+FROM
+
+  (SELECT
+     a.par_date,
+     a.plat,
+     c.category,
+     a.rid,
+     b.name,
+     b.room_content,
+     max(a.pcu)       AS pcu,
+     sum(a.duration)  AS duration,
+     max(a.rec_times) AS rec_times,
+     max(a.followers) AS followers,
+     max(a.weight)    AS weight
+   FROM panda_competitor.crawler_day_anchor_analyse a
+     LEFT JOIN
+     (select rid,plat,name,title as room_content,
+row_number() over(partition by rid,plat order by create_time desc) as rw 
+from panda_competitor.crawler_distinct_anchor 
+where par_date='${date}') b
+           ON b.rw=1 and a.rid = b.rid AND a.plat = b.plat
+     LEFT JOIN
+     (SELECT
+        plat,
+        rid,
+        category,
+        pcu,
+        row_number()
+        OVER (PARTITION BY plat, rid
+          ORDER BY pcu DESC) AS rw
+      FROM panda_competitor.crawler_day_anchor_analyse
+      WHERE par_date = '${date}') c
+       ON a.plat = c.plat AND a.rid = c.rid AND c.rw = 1
+   WHERE par_date = '${date}'
+   GROUP BY a.par_date, a.plat, c.category, a.rid, b.name,b.room_content) aa
+  LEFT JOIN
+  (
+
+select 
+par_date,
+plat,
+rid,
+pcu,
+duration,
+rec_times,
+followers,
+weight
+from panda_competitor_result.plat_anchor_rank
+where  par_date= '${date_sub}' 
+  ) bb
+    ON aa.plat = bb.plat AND aa.rid = bb.rid;
+"
+
+
+hive -e "
+insert overwrite table panda_competitor_result.plat_anchor_rank partition(par_date)
+select case when b.rid is not null then b.rid else c.rid end as rid,
+case when b.plat is not null then b.plat else c.plat end as plat,
+case when b.pcu is not null then b.pcu else c.pcu end,
+case when b.pcu_rank is not null then b.pcu_rank else c.pcu_rank end,
+case when b.duration is not null then b.duration else c.duration end, 
+case when b.duration_rank is not null then b.duration_rank else c.duration_rank end,
+case when b.weight is not null then b.weight else c.weight end,
+case when b.weight_rank is not null then b.weight_rank else c.weight_rank end,
+case when b.followers is not null then b.followers else c.followers end,
+case when b.followers_rank is not null then b.followers_rank else c.followers_rank end,
+case when b.rec_times is not null then b.rec_times else c.rec_times end,
+case when b.rec_times_rank is not null then b.rec_times_rank else c.rec_times_rank end,
+'${date}'
+from (
+select rid,
+plat,
+pcu,
+row_number()over(partition by plat order by pcu desc) as pcu_rank,
+duration,
+row_number()over(partition by plat order by duration desc) as duration_rank,
+weight,
+row_number()over(partition by plat order by weight desc) as weight_rank,
+followers,
+row_number()over(partition by plat order by followers desc) as followers_rank,
+rec_times,
+row_number()over(partition by plat order by rec_times desc) as rec_times_rank,
+'${date}' 
+from(
+select rid,
+plat,
+max(pcu) as pcu,
+sum(duration) duration,
+max(weight) as weight,
+max(followers)as followers,
+max(rec_times) as rec_times
+from panda_competitor.crawler_day_anchor_analyse
+where par_date='${date}'
+group by rid,plat
+) a) b
+full outer join( 
+select * from panda_competitor_result.plat_anchor_rank where par_date='${date_sub}'
+) c
+on b.plat=c.plat and c.rid=b.rid 
+;"
---
+++ shell/competitor_report/seven_insert.sh
@@ -0,0 +1,389 @@
+#!/bin/bash
+
+date=$1
+date=${date:=`date -d 'yesterday' +%Y%m%d`}
+date_sub=`date -d "-1day $date" +%Y%m%d`
+
+#主播PCU排行
+hive -e "
+insert overwrite table panda_competitor_result.anchor_pcu_rank partition(par_date)
+SELECT
+  b.id,
+  a.plat,
+  a.rid,
+  a.name,
+  a.pcu_plat_rank,
+  a.pcu,
+  a.weight,
+  a.fol,
+  nvl(cast(a.pcu_plat_rank - c.pcu_rank AS INT), '新上榜'),
+  a.room_content,
+  a.par_date
+FROM panda_competitor_result.crawler_anchor_day a
+  INNER JOIN panda_competitor.crawler_plat b ON a.plat = b.name
+  LEFT JOIN panda_competitor_result.plat_anchor_rank c
+    ON c.par_date = '${date_sub}' AND a.plat = c.plat AND a.rid = c.rid
+WHERE a.par_date = '${date}'
+      AND a.pcu_plat_rank >= 1 AND a.pcu_plat_rank <= 1000;
+"
+
+
+#主播变化趋势增减表
+hive -e "
+insert overwrite table panda_competitor_result.anchor_changed_rank partition(par_date)
+SELECT
+  b.id,
+  a.plat,
+  a.rid,
+  a.name,
+  a.category,
+  a.pcu,
+  nvl(cast(a.pcu_plat_rank - c.pcu_rank AS INT), '新上榜'),
+  a.livetime,
+  nvl(cast(a.livetime_plat_rank - c.duration_rank AS INT), '新上榜'),
+  a.weight,
+  nvl(cast(a.weight_plat_rank - c.weight_rank AS INT), '新上榜'),
+  a.fol,
+  a.par_date
+
+FROM panda_competitor_result.crawler_anchor_day a
+  INNER JOIN panda_competitor.crawler_plat b ON a.plat = b.name
+  LEFT JOIN panda_competitor_result.plat_anchor_rank c
+    ON c.par_date = '${date_sub}' AND a.plat = c.plat AND a.rid = c.rid 
+WHERE a.par_date = '${date}'
+      AND a.pcu_plat_rank >= 1 AND a.pcu_plat_rank <= 1000;
+"
+
+
+#主播订阅排行榜
+hive -e "
+insert overwrite table panda_competitor_result.anchor_fol_rank partition(par_date)
+SELECT
+  b.id,
+  a.plat,
+  a.rid,
+  a.name,
+  a.fol_plat_rank,
+  a.pcu,
+  a.weight,
+  a.fol,
+  nvl(cast(a.fol - c.followers AS INT), '新上榜'),
+  a.room_content,
+  a.par_date
+
+FROM panda_competitor_result.crawler_anchor_day a
+  INNER JOIN panda_competitor.crawler_plat b ON a.plat = b.name
+  LEFT JOIN  panda_competitor_result.plat_anchor_rank c
+    ON c.par_date = '${date_sub}' AND a.plat = c.plat AND a.rid = c.rid
+WHERE a.par_date = '${date}'
+      AND a.fol_plat_rank >= 1 AND a.fol_plat_rank <= 1000;
+"
+
+#主播订阅增长排行表
+hive -e "
+insert overwrite table panda_competitor_result.anchor_fol_up_rank partition(par_date)
+SELECT
+  b.id,
+  a.plat,
+  a.rid,
+  a.name,
+  d.fol_changed_plat_rank,
+  a.pcu,
+  a.weight,
+  a.fol,
+  d.fol_changed,
+  a.room_content,
+  a.par_date
+FROM panda_competitor_result.crawler_anchor_day a
+  INNER JOIN panda_competitor.crawler_plat b ON a.plat = b.name
+  INNER JOIN panda_competitor_result.crawler_anchor_change_day d
+    ON d.par_date = '${date}' AND a.plat = d.plat AND a.rid = d.rid
+WHERE a.par_date = '${date}'
+      AND d.fol_changed_plat_rank >= 1 AND d.fol_changed_plat_rank <= 1000;
+"
+
+
+#主播体重增长排行表
+hive -e "
+insert overwrite table panda_competitor_result.anchor_weight_rank partition(par_date)
+SELECT
+  b.id,
+  a.plat,
+  a.rid,
+  a.name,
+  a.weight_plat_rank,
+  a.pcu,
+  a.fol,
+  c.weight,
+  a.weight,
+  a.room_content,
+  a.par_date
+
+FROM panda_competitor_result.crawler_anchor_day a
+  INNER JOIN panda_competitor.crawler_plat b ON a.plat = b.name
+  LEFT JOIN panda_competitor_result.plat_anchor_rank c
+    ON c.par_date = '${date_sub}' AND a.plat = c.plat AND a.rid = c.rid 
+WHERE a.par_date = '${date}'
+      AND a.weight_plat_rank >= 1 AND a.weight_plat_rank <= 1000;
+"
+
+
+#主播播放时长排行表
+hive -e "
+insert overwrite table panda_competitor_result.anchor_livetime_rank partition(par_date)
+SELECT
+  b.id,
+  a.plat,
+  a.rid,
+  a.name,
+  a.livetime_plat_rank,
+  a.pcu,
+  a.fol,
+  a.livetime,
+  a.room_content,
+  a.par_date
+FROM panda_competitor_result.crawler_anchor_day a
+  INNER JOIN panda_competitor.crawler_plat b ON a.plat = b.name
+WHERE a.par_date = '${date}'
+      AND a.livetime_plat_rank >= 1 AND a.livetime_plat_rank <= 1000;
+"
+
+
+#推荐味日表
+hive -e "
+insert overwrite table panda_competitor_result.indexrec_day_report partition(par_date)
+SELECT
+  b.id,
+  a.plat,
+  a.rid,
+  a.name,
+  a.pcu,
+  a.fol,
+  d.fol_changed,
+  d.weight_changed,
+  a.livetime,
+  e.url,
+  a.par_date
+FROM panda_competitor_result.crawler_anchor_day a
+  INNER JOIN panda_competitor.crawler_plat b ON a.plat = b.name
+  INNER JOIN panda_competitor_result.crawler_anchor_change_day d
+    ON d.par_date = '${date}' AND a.plat = d.plat AND a.rid = d.rid
+  LEFT JOIN panda_competitor.crawler_distinct_anchor e
+    ON e.par_date = '${date}' AND a.rid = e.rid AND a.plat = e.plat AND a.category = e.category
+WHERE a.par_date = '${date}'
+      AND a.rectimes > 0;
+"
+
+#################################################################################################
+#2017-01-18新增
+#####
+#综合排名中间表
+
+hive -e "
+insert overwrite table panda_competitor_result.anchor_m_comprehensive_rank partition(par_date)
+
+select 
+case when d.rid is null then c.rid else d.rid end,
+case when d.plat is null then c.plat else d.plat end,
+case when d.comprehensive_score is null then c.comprehensive_score else d.comprehensive_score end,
+case when d.comprehensive_rank is null then c.comprehensive_rank else d.comprehensive_rank end,
+
+case when d.grow_score is null then c.grow_score else d.grow_score end,
+case when d.grow_rank is null then c.grow_rank else d.grow_rank end,
+
+'${date}'
+
+from(
+
+select a.rid,
+a.plat,
+
+(a.pcu/e.pcu_p)*e.pcu_w+
+(1-a.livetime/e.livetime_p)*e.livetime_w+
+(a.weight/e.weight_p)*e.weight_w+
+(a.fol/e.fol_p)*e.fol_w as comprehensive_score,
+
+row_number()over(
+partition by a.plat
+order by 
+--
+cast((a.pcu/e.pcu_p)*e.pcu_w+
+(1-a.livetime/e.livetime_p)*e.livetime_w+
+(a.weight/e.weight_p)*e.weight_w+
+(a.fol/e.fol_p)*e.fol_w as int) desc
+) as comprehensive_rank,
+
+
+((a.pcu-nvl(c.pcu,0))/f.pcu_p)*f.pcu_w+
+((a.weight-nvl(c.weight,0))/f.weight_p)*f.weight_w+
+((a.fol-nvl(c.followers,0))/f.fol_p)*f.fol_w grow_score,--总分
+
+row_number()over(
+partition by b.id
+order by 
+--
+cast(((a.pcu-nvl(c.pcu,0))/f.pcu_p)*f.pcu_w+
+((a.weight-nvl(c.weight,0))/f.weight_p)*f.weight_w+
+((a.fol-nvl(c.followers,0))/f.fol_p)*f.fol_w as int) desc
+) grow_rank,
+
+a.par_date
+
+
+FROM panda_competitor_result.crawler_anchor_day a
+  INNER JOIN panda_competitor.crawler_plat b ON a.plat = b.name
+  LEFT JOIN panda_competitor_result.plat_anchor_rank c
+    ON c.par_date = '${date_sub}' AND a.plat = c.plat AND a.rid = c.rid
+  left join 
+  (select * from panda_competitor_result.anchor_live_parameter where type=0) e
+  left join
+  (select * from panda_competitor_result.anchor_live_parameter where type=1) f
+WHERE a.par_date = '${date}') d
+
+full outer join
+(select * from panda_competitor_result.anchor_m_comprehensive_rank where par_date='${date_sub}' ) c
+on d.plat=c.plat and d.rid=c.rid;
+"
+
+
+
+
+
+ #主播综合排名
+hive -e "
+insert overwrite table panda_competitor_result.anchor_comprehensive_rank partition(par_date)
+select 
+--排名
+row_number()over(
+partition by b.id
+order by 
+--
+cast((a.pcu/e.pcu_p)*e.pcu_w+
+(1-a.livetime/e.livetime_p)*e.livetime_w+
+(a.weight/e.weight_p)*e.weight_w+
+(a.fol/e.fol_p)*e.fol_w as int) desc
+--
+),--总分排序
+
+------
+
+b.id,--平台id
+a.plat,--平台名称
+a.rid,--主播id
+a.name,--主播名字
+a.pcu,--pcu
+a.livetime,--开播时长
+
+
+cast(a.fol - nvl(c.followers,0) AS INT),--订阅增长
+cast(a.weight-nvl(c.weight,0)as int),--体重增量
+
+(a.pcu/e.pcu_p)*e.pcu_w,--pcu分数
+(1-a.livetime/e.livetime_p)*e.livetime_w,--开播时长分数
+
+(a.weight/e.weight_p)*e.weight_w,--体重增量分数
+(a.fol/e.fol_p)*e.fol_w,--订阅增量分数
+
+
+--总分
+(a.pcu/e.pcu_p)*e.pcu_w+
+(1-a.livetime/e.livetime_p)*e.livetime_w+
+(a.weight/e.weight_p)*e.weight_w+
+(a.fol/e.fol_p)*e.fol_w,
+
+row_number()over(
+partition by b.id
+order by 
+--
+cast((a.pcu/e.pcu_p)*e.pcu_w+
+(1-a.livetime/e.livetime_p)*e.livetime_w+
+(a.weight/e.weight_p)*e.weight_w+
+(a.fol/e.fol_p)*e.fol_w as int) desc
+--
+) -nvl(g.comprehensive_rank,0), --名次变化
+
+
+a.par_date
+FROM panda_competitor_result.crawler_anchor_day a
+  INNER JOIN panda_competitor.crawler_plat b ON a.plat = b.name
+  LEFT JOIN panda_competitor_result.plat_anchor_rank c
+    ON c.par_date = '${date_sub}' AND a.plat = c.plat AND a.rid = c.rid
+  left join 
+  (select * from panda_competitor_result.anchor_live_parameter where type=0) e
+  left join
+  (select * from panda_competitor_result.anchor_live_parameter where type=1) f
+  left join
+  (select * from panda_competitor_result.anchor_m_comprehensive_rank where par_date='${date_sub}') g
+on a.plat=g.plat and a.rid=g.rid
+WHERE a.par_date = '${date}';
+"
+
+
+#主播成长排名
+hive -e "
+insert overwrite table panda_competitor_result.anchor_growth_rank partition(par_date)
+select 
+row_number()over(
+partition by b.id
+order by 
+--
+cast(((a.pcu-nvl(c.pcu,0))/e.pcu_p)*e.pcu_w+
+((a.weight-nvl(c.weight,0))/e.weight_p)*e.weight_w+
+((a.fol-nvl(c.followers,0))/e.fol_p)*e.fol_w as int) desc
+--
+),--排序
+
+
+b.id,--平台id
+a.plat,--平台名称
+a.rid,--主播id
+a.name,--主播名称
+a.pcu-nvl(c.pcu,0),--pcu增值
+a.fol-nvl(c.followers,0),--订阅增值
+a.weight-nvl(c.weight,0),--体重增值
+((a.pcu-nvl(c.pcu,0))/e.pcu_p)*e.pcu_w,--pcu增值分数
+
+
+case when a.pcu=(a.pcu-nvl(c.pcu,0)) then 1 else 0 end,--pcu增值状态
+
+
+
+((a.weight-nvl(c.weight,0))/e.weight_p)*e.weight_w,--体重增量分数
+((a.fol-nvl(c.followers,0))/e.fol_p)*e.fol_w,--订阅增量分数
+
+((a.pcu-nvl(c.pcu,0))/e.pcu_p)*e.pcu_w+
+((a.weight-nvl(c.weight,0))/e.weight_p)*e.weight_w+
+((a.fol-nvl(c.followers,0))/e.fol_p)*e.fol_w,--总分
+
+row_number()over(
+partition by b.id
+order by 
+--
+cast(((a.pcu-nvl(c.pcu,0))/e.pcu_p)*e.pcu_w+
+((a.weight-nvl(c.weight,0))/e.weight_p)*e.weight_w+
+((a.fol-nvl(c.followers,0))/e.fol_p)*e.fol_w as int) desc
+--
+)-nvl(f.grow_rank,0),--名次变化
+a.par_date
+FROM panda_competitor_result.crawler_anchor_day a
+  INNER JOIN panda_competitor.crawler_plat b ON a.plat = b.name
+  LEFT JOIN panda_competitor_result.plat_anchor_rank c
+    ON c.par_date = '${date_sub}' AND a.plat = c.plat AND a.rid = c.rid
+  left join 
+  (select * from panda_competitor_result.anchor_live_parameter where type=1) e
+  left join
+  (select * from panda_competitor_result.anchor_m_comprehensive_rank where par_date='${date_sub}') f
+on a.plat=f.plat and a.rid=f.rid
+WHERE a.par_date = '${date}';"
+
+#####生成excel
+#成长
+/usr/local/jdk1.8.0_60/bin/java -jar /home/zhengbo/java-jar/export2excel.jar $date panda_competitor_result.anchor_comprehensive_rank par_date,rank,plat_id,plat,rid,name,puu,livetime,fol_up,weight_up,pcu_score,livetime_score,weight_score,fol_score,total_score,rank_change "日期,排序,平台ID,平台名称,主播ID,主播名称,PCU,开播时长,订阅增量,体重增量,PCU分数,开播时长分数,体重增量分数,订阅增量分数,总分,名次变化" /home/zhengbo/java-jar/file
+#增量
+/usr/local/jdk1.8.0_60/bin/java -jar /home/zhengbo/java-jar/export2excel.jar $date panda_competitor_result.anchor_growth_rank par_date,rank,plat_id,plat,rid,name,pcu_up,fol_up,weight_up,pcu_score,pcu_type,weight_up_score,fol_up_score,total_score,rank_change "日期,排序,平台ID,平台名称,主播ID,主播名称,PCU增值,订阅增量,体重增量,PCU增值分数,PCU增值状态,体重增量分数,订阅增量分数,总分,名次变化" /home/zhengbo/java-jar/file
+
+
+#发送邮件
+
+
+
